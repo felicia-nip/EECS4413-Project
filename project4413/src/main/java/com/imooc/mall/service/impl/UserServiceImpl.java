827e82da1e5727cd4fc7fc37a348e9c47050df0c
Yujie Tang <yujietang25@gmail.com>
2022-12-07 09:16:15 -0500

    EN localize ver 1.4

--- project4413/src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
+++ project4413/src/main/java/com/imooc/mall/service/impl/UserServiceImpl.java
@@ -1,18 +1,18 @@
 package com.imooc.mall.service.impl;
 
-import com.imooc.mall.common.Constant;
 import com.imooc.mall.exception.ImoocMallException;
 import com.imooc.mall.exception.ImoocMallExceptionEnum;
 import com.imooc.mall.model.dao.UserMapper;
 import com.imooc.mall.model.pojo.User;
 import com.imooc.mall.service.UserService;
 import com.imooc.mall.util.MD5Utils;
-import java.security.NoSuchAlgorithmException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
+
 /**
- * 描述：     UserService实现类
+ * UserService implementation class
  */
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,28 +21,33 @@ public class UserServiceImpl implements UserService {
     UserMapper userMapper;
 
     @Override
+    /**
+     * find user by they primary key
+     */
     public User getUser() {
         return userMapper.selectByPrimaryKey(1);
     }
 
     @Override
     public void register(String userName, String password, String emailAddress) throws ImoocMallException {
-        //查询用户名是否存在，不允许重名
+        //Check whether the username exists, no duplicate names are allowed
         User result = userMapper.selectByName(userName);
         if (result != null) {
             throw new ImoocMallException(ImoocMallExceptionEnum.NAME_EXISTED);
         }
 
-        //写到数据库
+        //update to the database
         User user = new User();
         user.setUsername(userName);
         user.setEmailAddress(emailAddress);
         try {
+//            encrypt password by using MD5
             user.setPassword(MD5Utils.getMD5Str(password));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         int count = userMapper.insertSelective(user);
+//        it should be 1 after we did insert, 0 means insertion failed
         if (count == 0) {
             throw new ImoocMallException(ImoocMallExceptionEnum.INSERT_FAILED);
         }
@@ -65,8 +70,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updateInformation(User user) throws ImoocMallException {
-        //更新个性签名
+        //Update personality signature
         int updateCount = userMapper.updateByPrimaryKeySelective(user);
+//        it should only change one thing! which is current user's signature
         if (updateCount > 1) {
             throw new ImoocMallException(ImoocMallExceptionEnum.UPDATE_FAILED);
         }
@@ -74,16 +80,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean checkAdminRole(User user) {
-        //1是普通用户，2是管理员
+        //1 is the consumer , 2 is the administrator
         return user.getRole().equals(2);
     }
 
     @Override
     public boolean checkEmailRegistered(String emailAddress) {
         User user = userMapper.selectOneByEmailAddress(emailAddress);
-        if (user != null) {
-            return false;
-        }
-        return true;
+//        user is not null, only if the email exists.
+        return user == null;
     }
 }
