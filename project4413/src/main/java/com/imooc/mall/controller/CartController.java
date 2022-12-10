f1bca2349d837c2aac0af159731a485028ec7120
ty <yujietang25@gmail.com>
2022-12-10 17:29:57 -0500

    eng 1.8

--- project4413/src/main/java/com/imooc/mall/controller/CartController.java
+++ project4413/src/main/java/com/imooc/mall/controller/CartController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 描述：     购物车Controller
+ * Shopping Cart Controller
  */
 @RestController
 @RequestMapping("/cart")
@@ -26,22 +26,22 @@ public class CartController {
     CartService cartService;
 
     @GetMapping("/list")
-    @ApiOperation("购物车列表")
+    @ApiOperation("Shopping Cart List")
     public ApiRestResponse list() {
-        //内部获取用户ID，防止横向越权
+        //Obtain user IDs internally to prevent Broken access control
         List<CartVO> cartList = cartService.list(UserFilter.userThreadLocal.get().getId());
         return ApiRestResponse.success(cartList);
     }
 
     @PostMapping("/add")
-    @ApiOperation("添加商品到购物车")
+    @ApiOperation("Add products to cart")
     public ApiRestResponse add(@RequestParam Integer productId, @RequestParam Integer count) {
         List<CartVO> cartVOList = cartService.add(UserFilter.userThreadLocal.get().getId(), productId, count);
         return ApiRestResponse.success(cartVOList);
     }
 
     @PostMapping("/update")
-    @ApiOperation("更新购物车")
+    @ApiOperation("Update Shopping Cart")
     public ApiRestResponse update(@RequestParam Integer productId, @RequestParam Integer count) {
         List<CartVO> cartVOList = cartService
                 .update(UserFilter.userThreadLocal.get().getId(), productId, count);
@@ -49,26 +49,26 @@ public class CartController {
     }
 
     @PostMapping("/delete")
-    @ApiOperation("删除购物车")
+    @ApiOperation("Delete Shopping Cart")
     public ApiRestResponse delete(@RequestParam Integer productId) {
-        //不能传入userID，cartID，否则可以删除别人的购物车
+        //Can't pass in userID, cartID, otherwise you can delete other people's shopping cart
         List<CartVO> cartVOList = cartService.delete(UserFilter.userThreadLocal.get().getId(), productId);
         return ApiRestResponse.success(cartVOList);
     }
 
     @PostMapping("/select")
-    @ApiOperation("选择/不选择购物车的某商品")
+    @ApiOperation("Select/unselect an item in the shopping cart")
     public ApiRestResponse select(@RequestParam Integer productId, @RequestParam Integer selected) {
-        //不能传入userID，cartID，否则可以删除别人的购物车
+        //Can't pass in userID, cartID, otherwise you can delete other people's shopping cart
         List<CartVO> cartVOList = cartService
                 .selectOrNot(UserFilter.userThreadLocal.get().getId(), productId, selected);
         return ApiRestResponse.success(cartVOList);
     }
 
     @PostMapping("/selectAll")
-    @ApiOperation("全选择/全不选择购物车的某商品")
+    @ApiOperation("Select all/select none of an item in the shopping cart")
     public ApiRestResponse selectAll(@RequestParam Integer selected) {
-        //不能传入userID，cartID，否则可以删除别人的购物车
+        //Can't pass in userID, cartID, otherwise you can delete other people's shopping cart
         List<CartVO> cartVOList = cartService
                 .selectAllOrNot(UserFilter.userThreadLocal.get().getId(), selected);
         return ApiRestResponse.success(cartVOList);
